cb97fcbe2b40b113348bca604beb9594f65165f1
nasta889 <161520096+nasta889@users.noreply.github.com>
2024-11-20 14:34:42 +0200

    Update Product.java

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,2 +1,30 @@
 package PACKAGE_NAME;public class Product {
 }
+public class Product {
+    private int id;
+    private String name;
+    private double price;
+
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return id + ". " + name + " - " + price + " грн";
+    }
+}
