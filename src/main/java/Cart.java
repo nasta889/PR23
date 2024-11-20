fcc44f2d2f6bf878d02295418cca85e9e965b012
nasta889 <161520096+nasta889@users.noreply.github.com>
2024-11-20 14:35:05 +0200

    Update Cart.java

--- src/main/java/Cart.java
+++ src/main/java/Cart.java
@@ -1,2 +1,33 @@
 package PACKAGE_NAME;public class Cart {
 }
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+    private List<Product> products;
+
+    public Cart() {
+        products = new ArrayList<>();
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void viewCart() {
+        if (products.isEmpty()) {
+            System.out.println("Кошик порожній.");
+        } else {
+            System.out.println("Товари в кошику:");
+            double total = 0;
+            for (Product product : products) {
+                System.out.println(product);
+                total += product.getPrice();
+            }
+            System.out.println("Загальна сума: " + total + " грн");
+        }
+    }
+
+    public void clearCart() {
+        products.clear();
+    }
